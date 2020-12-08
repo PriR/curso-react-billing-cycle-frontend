0f10112b08539ebc72e530fafc5000fff5a60ec0
Priscila Silva <priscila.rodrigues@hotmail.com.br>
2020-12-08 19:10:15 +0100

    Create content, content header, routes and grid component

---
+++ src/common/layout/grid.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+export default class Grid extends Component {
+  toCssClasses(numbers) {
+    const cols = numbers ? numbers.split(' ') : [];
+    const classes = '';
+    if (cols[0]) classes += `cols-xs-${cols[0]}`; // dispositivos muito pequenos
+    if (cols[1]) classes += `cols-sm-${cols[1]}`; // dispositivos pequenos
+    if (cols[2]) classes += `cols-md-${cols[2]}`; // dispositivos médios
+    if (cols[3]) classes += `cols-lg-${cols[3]}`; // dispositivos grandes
+
+    return classes;
+  }
+
+  render() {
+    const gridClasses = this.toCssClasses(this.props.cols || '12'); // se valor não for fornecido o padrão é 12
+    return (
+      <div className={gridClasses}>
+        {this.props.children}
+      </div>
+    )
+  }
+}
--- src/main/app.jsx
+++ src/main/app.jsx
@@ -3,13 +3,14 @@ import '../common/template/dependencies';
 import Header from '../common/template/header';
 import SideBar from '../common/template/sidebar';
 import Footer from '../common/template/footer'
+import Routes from './routes'
 
 export default props => (
   <div className='wrapper'>
     <Header />
     <SideBar />
     <div className='content-wrapper'>
-      <h1>Conteúdo</h1>
+      <Routes />
     </div>
     <Footer />
   </div>
