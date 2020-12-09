adc53db8a27598235229820554ba515478263626
Priscila Silva <priscila.rodrigues@hotmail.com.br>
2020-12-09 14:29:29 +0100

    Create tabContent and use in billinCycle

--- src/billingCycle/billingCycle.jsx
+++ src/billingCycle/billingCycle.jsx
@@ -6,6 +6,7 @@ import Tabs from '../common/tab/tabs'
 import TabsHeader from '../common/tab/tabsHeader'
 import TabsContent from '../common/tab/tabsContent'
 import TabHeader from '../common/tab/tabHeader'
+import TabContent from '../common/tab/tabContent'
 export default class BillingCycle extends Component {
   render() {
     return (
@@ -20,7 +21,10 @@ export default class BillingCycle extends Component {
               <TabHeader label='Excluir' icon='trash-o' target='tabDelete' />
             </TabsHeader>
             <TabsContent>
-              
+              <TabContent id='tabList'><h1>Lista</h1></TabContent>
+              <TabContent id='tabCreate'><h1>Incluir</h1></TabContent>
+              <TabContent id='tabUpdate'><h1>Alterar</h1></TabContent>
+              <TabContent id='tabDelete'><h1>Excluir</h1></TabContent>
             </TabsContent>
           </Tabs>
         </Content>
