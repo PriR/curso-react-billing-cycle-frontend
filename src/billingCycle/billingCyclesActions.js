244afc9f79cef973ecd9ac147eacb0c5ffb73e53
Priscila Silva <priscila.rodrigues@hotmail.com.br>
2020-12-16 05:56:34 +0100

    Do other actions after success on posting

--- src/billingCycle/billingCyclesActions.js
+++ src/billingCycle/billingCyclesActions.js
@@ -1,5 +1,8 @@
 import axios from 'axios';
 import { toastr } from 'react-redux-toastr';
+import { reset, reset as resetForm } from 'redux-form';
+import { showTabs, selectTab } from '../common/tab/tabActions';
+
 const BASE_URL = 'http://localhost:3004/api';
 
 export function getList() {
@@ -11,16 +14,20 @@ export function getList() {
 }
 
 export function create(values) {
-  console.log('chamou create');
-  axios
-    .post(`${BASE_URL}/billingCycles`, values)
-    .then(resp => {
-      toastr.success('Sucesso', 'Operação realizada com sucesso');
-    })
-    .catch(e => {
-      e.response.data.errors.forEach(error => toastr.error('Erro', error));
-    });
-  return {
-    type: 'TEMP',
+  return dispatch => {
+    axios
+      .post(`${BASE_URL}/billingCycles`, values)
+      .then(resp => {
+        toastr.success('Sucesso', 'Operação realizada com sucesso');
+        dispatch([
+          resetForm('billingCycleForm'),
+          getList(),
+          selectTab('tabList'),
+          showTabs('tabList', 'tabCreate'),
+        ]); // redux-multi utilizado para enviar array no dispatch
+      })
+      .catch(e => {
+        e.response.data.errors.forEach(error => toastr.error('Erro', error));
+      });
   };
 }
